556e0c95f0c9a3ed3e96fc75951f1a39a029c6d0
Potato Leong <smithchoon@gmail.com>
2018-08-21 11:22:04 +0800

    Add files via upload

---
+++ Execrise10.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP_Looping_Execrise</title>
+</head>
+<body>
+	<?php
+	$input =  15; // Please enter your input here
+	echo " Your input is $input <br> ";
+	if ($input >= 0){
+		for ($a = 0; $a <= 15; $a++){
+			echo "Your number is $a <br>";
+		}
+	}else{
+		echo " Your echo is negative "; 	
+	}
+	?>
+</body>
+</html>
